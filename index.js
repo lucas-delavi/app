1ef729122ee3a319b051ae4cc641994a005ae969
Lucas Delavi <128806753+lucas-delavi@users.noreply.github.com>
2024-09-11 16:19:40 -0300

    inicia o menu da aplicação

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-// hello world
-let mensagem = "hello worldo"
-console.log(mensagem);
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+
+    while(true){
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+
+        switch (opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")               
+                break
+        
+            case "listar":
+                console.log("vamos listar")
+                break
+            
+            case "sair":
+                return
+        }
+    }
+}
+
+start()
