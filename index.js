180a416fae10a74632a921661a8a931267843c3e
Lucas Delavi <128806753+lucas-delavi@users.noreply.github.com>
2024-09-13 10:39:37 -0300

    add metas abertas

--- index.js
+++ index.js
@@ -58,8 +58,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0){
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+
+    await select({
+        message: "Metas abertas " + abertas.length,
+        choices:[...abertas]
     })
 }
 
@@ -83,6 +99,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -99,8 +119,13 @@ const start = async () => {
             case "listar":
                 await listarMetas()
                 break
+
             case "realizadas":
                 await metasRealizadas()
+                break
+
+            case "abertas":
+                await metasAbertas()
                 break
             
             case "sair":
