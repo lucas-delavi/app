3121c12c032e12016a21291f3c35a408063ccf30
Lucas Delavi <128806753+lucas-delavi@users.noreply.github.com>
2024-09-13 10:15:52 -0300

    Metas realizadas

--- index.js
+++ index.js
@@ -47,6 +47,22 @@ const listarMetas = async () => {
     console.log ('Meta(s) marcadas como concluída(s)')
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if (realizadas.length == 0) {
+        console.log('Não existem metas realizadas! :(')
+        return
+    }
+
+    await select({
+        message: "Metas realizadas",
+        choices: [...realizadas]
+    })
+}
+
 const start = async () => {
 
     while(true){
@@ -61,6 +77,10 @@ const start = async () => {
                 {
                     name: "Listar metas",
                     value: "listar"
+                },
+                {
+                    name: "Metas realizadas",
+                    value: "realizadas"
                 },
                 {
                     name: "Sair",
@@ -78,6 +98,9 @@ const start = async () => {
         
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             
             case "sair":
